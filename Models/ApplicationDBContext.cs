9a0bda7fc8f7bfc39a2ca77b2df85d72960a4bec
nastiandru <80123718+nastiandru@users.noreply.github.com>
2021-12-17 17:42:04 +0200

    Update ApplicationDBContext.cs

--- Models/ApplicationDBContext.cs
+++ Models/ApplicationDBContext.cs
@@ -5,8 +5,31 @@ using System.Threading.Tasks;
 
 namespace Prog_Lab1.Models
 {
-    public class ApplicationDBContext
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
     {
-       
-    }
+        public
+       AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+        : base(options) { }
+        public static class IdentitySeedData
+        {
+            private const string adminUser = "Admin";
+            private const string adminPassword = "Secret123$";
+            public static async void EnsurePopulated(IApplicationBuilder app)
+            {
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    var userManager = (UserManager<IdentityUser>)scope
+
+                   .ServiceProvider.GetService(typeof(UserManager<IdentityUser>));
+                    IdentityUser user = await
+                   userManager.FindByIdAsync(adminUser);
+                    if (user == null)
+                    {
+                        user = new IdentityUser(adminUser);
+                        await userManager.CreateAsync(user, adminPassword);
+                    }
+                }
+            }
+        }
+    } 
 }
