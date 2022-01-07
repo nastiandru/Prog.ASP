ebefc55e384da9190e39f285a78ede052c4b4182
nastiandru <80123718+nastiandru@users.noreply.github.com>
2022-01-07 17:32:51 +0200

    Update ApiBlogController.cs

--- Controllers/ApiBlogController.cs
+++ Controllers/ApiBlogController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,80 @@ using System.Threading.Tasks;
 
 namespace Prog_Lab1.Controllers
 {
+    [Route("api/items")]
     public class ApiBlogController : Controller
     {
-        public IActionResult Index()
+        private ICRUDBlogItemRepository items;
+
+        public ApiBlogController(ICRUDBlogItemRepository items)
         {
-            return View();
+            this.items = items;
         }
+
+        [HttpGet]
+        public List<BlogItem> GetAll()
+        {
+            return items.FindAll();
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public BlogItem GetOne(int id)
+        {
+            BlogItem blogItem = items.FindById(id);
+            if(blogItem = null //  зробити не дорівнює
+            {
+                return new OkObjectResult(blogItem);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpPost]
+        public ActionResult Add([FromBody] BlogItem item)
+        {
+            if (ModelState.IsValid)
+            {
+                BlogItem blogItem = items.Save(item);
+                return new CreatedrESULT($"/API/ITEMS/{blogItem.Id}", blogItem);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete]
+        [Route("{id}")]
+
+        public ActionResult Delete(int id)
+        {
+            if (items.FindById(id) == null) // то саме
+            {
+                items.Delete(id);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpPut]
+        [Route("{id}")]
+
+        public ActionResult Update(int id, [FromBody] BlogItem item)
+        {
+            item.iD = id;
+            BlogItem blogitem = items.Update(item);
+            if(blogitem ==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return new OkObjectResult(blogItem);// можна return NoContent();
+            }
+        }
+
     }
+
 }
